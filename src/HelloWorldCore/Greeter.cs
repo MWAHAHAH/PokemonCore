0e9a5de02d37f300168efe245cf416aaf31f4c95
matteo moscatelli <matteomoscatelli@hotmail.it>
2017-03-03 13:10:46 +0100

    01.07

---
+++ src/HelloWorldCore/Greeter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace HelloWorldCore
+{
+    public interface IGreeter
+    {
+        string GetGreeting();
+    }
+
+    public class Greeter : IGreeter
+    {
+        private string _greeting;
+
+        public Greeter(IConfiguration config)
+        {
+            _greeting = config["Greeting"];
+        }
+
+        public string GetGreeting()
+        {
+            return _greeting;
+        }
+    }
+}
